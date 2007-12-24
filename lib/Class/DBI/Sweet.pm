8580cf0b470feb9c934ab3713cff2ef06bc6cb07
(no author) <(no author)@8106dff3-0242-0410-9b64-ac28c09226a2>
2007-12-24 08:04:42 +0000

    start 0.10 dev cycle
    git-svn-id: https://www.redhotpenguin.com/svn/class_dbi_sweet/trunk@12 8106dff3-0242-0410-9b64-ac28c09226a2

--- lib/Class/DBI/Sweet.pm
+++ lib/Class/DBI/Sweet.pm
@@ -25,7 +25,7 @@ else {
 
 our $UUID_Is_Available = ( $@ ? 0 : 1 );
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 #----------------------------------------------------------------------
 # RETRIEVING
